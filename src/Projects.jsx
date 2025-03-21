828f0ed8c2ce0af5c38649041f97ff300e56864d
HugoB. <huguinhobastos@hotmail.com>
2025-03-21 21:15:21 +0000

    Projects section done

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -2,7 +2,6 @@ import MyToDoListPic from './assets/MyToDoListPic.png';
 import BabyMaterPic from './assets/BabyMaterPic.png';
 import AmazonClonePic from './assets/AmazonClonePic.png';
 import RockPaperScissorsPic from './assets/RockPaperScissorsPic.png';
-import ProjectModal from './ProjectModal';
 
 //Lista com os meus projetos
 const projects = [
@@ -60,43 +59,44 @@ const Projects = () => {
               ></div>
 
               {/* Gradient escuro (desaparece no hover) */}
-              <div className="absolute inset-0 bg-[#141419] bg-opacity-10 transition-opacity duration-300 group-hover:bg-opacity-90 group-focus-within:bg-opacity-0"></div>
+              <div className="absolute inset-0 bg-[#141419] bg-opacity-10 transition-opacity duration-300 group-hover:bg-opacity-90 group-focus-within:bg-opacity-90"></div>
 
               {/* Conteúdo do card */}
               {/* <div className="relative flex flex-col items-center justify-center h-full py-12 px-2 gap-8"> */}
               <div className="relative flex flex-col items-center justify-center min-h-72 py-4 px-4 gap-4 group">
 
                 {/* Titulo do card */}
-                <h3 className="text-2xl text-center font-semibold opacity-0 transition-opacity duration-300 drop-shadow group-hover:opacity-100">
+                <h3 className="text-2xl text-blue-200 text-center font-semibold opacity-0 transition-opacity duration-300 drop-shadow group-hover:opacity-100 group-focus-within:opacity-100">
                   {project.title}
                 </h3>
 
-                <p className='opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center'>{project.description}</p>
+                <p className='opacity-0 group-hover:opacity-100 group-focus-within:opacity-100 transition-opacity duration-300 text-center'>{project.description}</p>
 
-                <div className="flex flex-wrap justify-center gap-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 mt-2 mb-3">
+                <div className="flex flex-wrap justify-center gap-2 opacity-0 group-hover:opacity-100 group-focus-within:opacity-100 transition-opacity duration-300 mt-2 mb-3">
                   {project.usedTech.map((tech) => (
-                    <span className=' text-[rgba(20,20,25)] font-semibold rounded py-1 px-2 underline-gradient'>{tech}</span>
+                    <span className=' text-[rgba(20,20,25)] font-semibold rounded py-1 px-2 underline-brush'>{tech}</span>
                   ))}
                 </div>
 
                 {/* Botões (aparecem no hover) */}
                 <div className="flex flex-wrap justify-center gap-4 opacity-0 transition-opacity duration-300 group-hover:opacity-100 group-focus-within:opacity-100">
                   <a
-                    href={project.linkRepo}
-                    tabIndex={0}
-                    className="bg-blue-200 text-[#141419] font-semibold px-6 py-2 border-2 rounded-md drop-shadow hover:bg-white focus:bg-blue-300 transition"
-                  >
-                    Repository
-                  </a>
-                  <a
                     href={project.linkDemo}
                     tabIndex={0}
                     target='_blank'
                     rel="noopener noreferrer"
-                    className="bg-[#141419] text-white font-semibold px-6 py-2 border-2 rounded-md drop-shadow hover:bg-white hover:text-[#141419] focus:bg-gray-800 transition"
+                    className="bg-[#141419] text-white font-semibold text-center w-32 px-6 py-2 border-2 rounded-md drop-shadow hover:bg-white hover:text-[#141419] focus-visible:bg-white focus-visible:text-[#141419] transition"
                   >
                     Live
                   </a>
+                  <a
+                    href={project.linkRepo}
+                    tabIndex={0}
+                    className="bg-[#141419] font-semibold text-center w-32 px-6 py-2 border-2 rounded-md drop-shadow hover:bg-white hover:text-[#141419] focus-visible:bg-white focus-visible:text-[#141419] transition"
+                  >
+                    Repository
+                  </a>
+                  
                 </div>
               </div>
             </article>
