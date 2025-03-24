817c296f6c4eafc904eee80b14747c477a2f1cce
HugoB. <huguinhobastos@hotmail.com>
2025-03-24 01:01:05 +0000

    Navbar done

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -36,11 +36,9 @@ const NavBar = () => {
   }, []);
 
   return (
-    <>
-    <nav className="fixed top-0 left-0 right-0 z-10 max-w-full sm:max-w-[85%] xl:max-w-[75%] mx-auto">
+    <nav className="fixed top-0 left-0 right-0 z-20 max-w-full sm:max-w-[85%] xl:max-w-[75%] mx-auto">
 
       <div className="flex justify-between items-center h-20 bg-[#141419] px-4">
-
         <div className="text-white text-2xl font-bold icon-pulsing">
           <a href="#contact" className="flex items-center">
             <img src={profilePicRightCartoon} alt="Hire Me Button" className="h-16" />
@@ -64,14 +62,23 @@ const NavBar = () => {
           </li>
         </ul>
 
-        <div className="md:hidden flex">
+        {/* Mobile Menu com traços normais */}
+        {/* <div className="md:hidden flex">
           <button className="group" aria-label="Menu" onClick={() => setMobileMenu(!mobileMenu)}>
             <span className={`block w-6 h-1 bg-white group-hover:bg-gray-300 group-focus-visible:bg-gray-300 ${mobileMenu ? "rotate-45 translate-y-1/2" : ""} transition-transform duration-300`}></span>
             <span className={`block w-6 h-1 bg-white group-hover:bg-gray-300 group-focus-visible:bg-gray-300 mt-1 mb-1 ${mobileMenu ? "hidden" : ""}`}></span>
             <span className={`block w-6 h-1 bg-white group-hover:bg-gray-300 group-focus-visible:bg-gray-300 ${mobileMenu ? "-rotate-45 -translate-y-1/2" : ""} transition-transform duration-300`}></span>
           </button>
-        </div>
+        </div> */}
 
+        {/* Mobile Menu com os divisores usados ao longo da página (mas com cor branca)*/}
+        <div className="md:hidden flex">
+          <button className="group w-8" aria-label="Menu" onClick={() => setMobileMenu(!mobileMenu)}>
+            <div className={`h-1 bg-gradient-to-r from-transparent via-white to-transparent ${mobileMenu ? "rotate-45 translate-y-1/2" : ""} transition-transform duration-300`}></div>
+            <div className={`h-1 my-1 bg-gradient-to-r from-transparent via-white to-transparent ${mobileMenu ? "h-0 my-0" : ""} transition-all duration-300`}></div>
+            <div className={`h-1 bg-gradient-to-r from-transparent via-white to-transparent ${mobileMenu ? "-rotate-45 -translate-y-1/2" : ""} transition-transform duration-300`}></div>
+          </button>
+        </div>
       </div>
 
       {/* Sidebar menu para o mobile, abrir com o hamburguer menu  */}
@@ -95,8 +102,7 @@ const NavBar = () => {
       } */}
 
       {/* Fullbar menu para o mobile, abrir com o hamburguer menu  */}
-      {mobileMenu &&
-      <ul className={`md:hidden flex flex-col items-center justify-center gap-12 text-2xl z-10 bg-[#141419] opacity-95 fixed right-0 left-0 top-20 bottom-0 px-4 py-6 ${mobileMenu ? "top-20" : ""} transition-all duration-300 ease-in-out`}>
+      <ul className={`md:hidden flex flex-col items-center justify-center gap-12 text-2xl -z-10 bg-[#141419] opacity-95 fixed w-full left-0 top-0 bottom-0 px-4 py-6 ${mobileMenu ? "top-20" : "-translate-y-full"} transition-all duration-300 ease-in-out`}>
         <li className={`relative after:content-[''] after:absolute after:left-1/2 after:-bottom-[25%] after:h-[1px] ${activeSection === "aboutMe" ? "after:w-[33%]" : "after:w-[0%]" } after:bg-gradient-to-r after:from-transparent after:via-gray-300 after:to-transparent after:transition-[width] after:duration-300 after:ease-in-out after:-translate-x-1/2 hover:after:w-[66%] focus-within:after:w-[66%]`}>
           <a href="#aboutMe" className="hover:text-gray-300 focus-visible:text-gray-300" onClick={() => setMobileMenu(false)}>About Me</a>
         </li>
@@ -110,12 +116,8 @@ const NavBar = () => {
           <a href="#contact" className="hover:text-gray-300 focus-visible:text-gray-300" onClick={() => setMobileMenu(false)}>Contact</a>
         </li>
       </ul>
-      }
 
     </nav>
-
-      
-  </>
   )
 }
 
